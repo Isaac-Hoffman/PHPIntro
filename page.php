d547e34e1d110eb714002136cc757744dcc8cf7a
unknown <19hoffmani@gmail.com>
2019-01-23 14:41:27 -0600

    Practice

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php
+	//echo ("Hello World"); print statements in php
+	//Isaac Hoffman
+	/*multiline start with the backslash and end with astrisks 
+	*/
+	$greeting = "Hello Greeting";
+	echo ($greeting);
+	//there are 6 data types in php
+	$count = 6; //integer
+	$price = 10.50; //double
+	$first_name = "Isaac"; //string
+	$in_boolean = true; //or false lower case
+	// also array and objects
+	define('PI', 3.14);
+	echo (PI);
+	
+	//concatenation
+	
+	echo('Hello, ' . $first_name);
+	echo("Hello, $first_name");
